97dd9f2f4110c651fa81355c4fabdb228ffe9402
Augusto Otto <augustomolke@gmail.com>
2024-07-21 09:13:46 -0300

    remove gasp

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import App from './App.svelte'
 import './index.css'
+import * as mockApi from "./mock/mockApi";
+
+
+if (process.env.NODE_ENV === "development") {
+  globalThis.google ={script:{run: {mockApi}}};
+}
 
 const app = new App({
   target: document.body,
---
+++ src/mock/mockApi.js
@@ -0,0 +1,16 @@
+export function getActiveUserEmail() {
+    return "thinkle@example.com";
+  }
+  
+  export function testMe() {
+    return 17;
+  }
+  
+  export function foo() {
+    return 17;
+  }
+  
+  
+  export function testeMock(s) {
+    return null; // TODO: Replace with mock return value of type string[]
+  }
